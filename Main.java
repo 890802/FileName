4f818108616ce818766bc9586a0eafc8814cff33
890802 <hi.0802@yahoo.com>
2017-07-07 14:19:31 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,14 +11,16 @@ public class Main {
         String prefix = new String();
         String path = new String();
         String newName = new String();
+        String fileType = new String();
+        String workingPath = new String();
         int files;
         int groups;
 
         System.out.println("Folder Path: ");
         path = input.next();
 
-        System.out.println("File Group Prefix: ");
-        prefix = input.next();
+        /*System.out.println("File Group Prefix: ");
+        prefix = input.next();*/
 
         System.out.println("Number of Groups: ");
         groups = input.nextInt();
@@ -26,18 +28,80 @@ public class Main {
         System.out.println("Files per Group: ");
         files = input.nextInt();
 
-        /*Navigate to Path + Catch Error*/
+        System.out.println("FileType: ");
+        fileType = input.next();
 
-        if(/*File Prefix Match*/){
+        //Modification
 
-            for(int i = 1; i <= groups; i++){
+        if (fileType.substring(0,1).compareTo(".") == 0){
 
-                System.out.println("Enter New File Prefix: ");
-                newName = input.next();
+            fileType = fileType.substring(1);
 
-                for(int j = 1; j <=files; j++){
+        }
+
+        if(path.substring(path.length() - 1).compareTo("/") == 0 ||path.substring(path.length() - 1).compareTo("\\") == 0 ){
+
+            path = path.substring(0,path.length() - 1);
+            System.out.println(path);
+
+        }
+
+        File dir = new File(path);
+
+        //Renaming
+
+        if(dir.isDirectory()) {
+
+            int it = 1;
+
+            for (File old : dir.listFiles()) {
+
+                if(old.isDirectory() == false) {
+
+                    if (it == 1) {
 
 
+                        System.out.println("Prefix: ");
+                        newName = input.next();
+                        File makeDir = new File(path + "/" + newName);
+                        workingPath = path + "/" + newName;
+                        makeDir.mkdir();
+
+                    }
+
+                    if (it < files) {
+
+                        try {
+                            File newFile = new File(workingPath + "/" + newName + it + "." + fileType);
+
+                            if (old.renameTo(newFile)) {
+                                System.out.println("Rename successful");
+                            } else {
+                                System.out.println("Rename failed");
+                            }
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                            e.printStackTrace();
+                        }
+                        it++;
+
+                    } else {
+
+                        try {
+                            File newFile = new File(workingPath + "/" + newName + it + "." + fileType);
+
+                            if (old.renameTo(newFile)) {
+                                System.out.println("Rename successful");
+                            } else {
+                                System.out.println("Rename failed");
+                            }
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                            e.printStackTrace();
+                        }
+                        it = 1;
+
+                    }
 
                 }
 
@@ -46,7 +110,7 @@ public class Main {
         }
         else{
 
-            System.out.println("Prefix not Found");
+            System.out.println("Path not Found");
 
         }
     }
